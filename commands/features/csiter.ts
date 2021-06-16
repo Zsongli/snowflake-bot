8a09c2bf1fd9b173c94a6e8f337168152b3afcf0
Zsong Lee <zsongli@gmail.com>
2021-06-16 19:08:29 +0200

    unimportant fix

--- commands/features/csiter.ts
+++ commands/features/csiter.ts
@@ -8,11 +8,9 @@ export const messageCommand: MessageCommand = {
     desc: "A csítert a helyére küldi (azaz Zsoltit), ezt csak adminok tehetik meg vele. Exkluzív a The Snowflake Team szerveréhez.",
     permission: (msg: Discord.Message) => msg.guild!.id === "701483086994735264",
     run: (msg: Discord.Message) => {
-        if (msg.member?.permissions.has("ADMINISTRATOR"))
-            return bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.setChannel("793149257455501312").catch(() => { msg.channel.send("Nincs bent egyik hangcsatornában sem :(") });
-
-        msg.channel.send("Ezt csak adminok használhatják! Kinek képzeled magad, hm?");
-
+        if (!msg.member?.hasPermission("ADMINISTRATOR")) return msg.channel.send("Ezt csak adminok használhatják! Kinek képzeled magad, hm?");
+        if (!bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.channel) return msg.channel.send(msg.member.toString() + ", Nincs bent egyik hangcsatornában sem :(");
+        bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.setChannel("793149257455501312");
     },
     onNoPerm: (msg: Discord.Message) => {
         msg.channel.send("That command exclusive to another server.");
@@ -25,7 +23,7 @@ export const slashCommand: SlashCommand = {
     desc: "A csítert a helyére küldi (azaz Zsoltit), ezt csak adminok tehetik meg vele.",
     permission: (interaction) => interaction.member.hasPermission("ADMINISTRATOR"),
     run: (interaction) => {
-        if(!bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.channel) return interaction.channel.send(interaction.member.toString() + ", Nincs bent egyik hangcsatornában sem :(");
+        if (!bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.channel) return interaction.channel.send(interaction.member.toString() + ", Nincs bent egyik hangcsatornában sem :(");
 
         interaction.AcknowledgeReply("Kapta.");
         bot.guilds.cache.get("701483086994735264")?.members.cache.get("569153209927729176")?.voice.setChannel("793149257455501312");
